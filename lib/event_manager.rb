f014046fda77ae203fae0cfc258ed784f617f05e
yungProg <gboakye007@outlook.com>
2025-02-26 17:56:59 +0000

    iteration 4 lessons

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,31 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+template_letter = File.read('form_letter.html')
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+    begin
+        legislators = civic_info.representative_info_by_address(
+            address: zipcode,
+            levels: 'country',
+            roles: ['legislatorUpperBody', 'legislatorLowerBody']
+            )
+        
+        legislators = legislators.officials
+
+        legislator_names = legislators.map(&:name)
+
+        legislators_string = legislator_names.join(', ')
+    rescue 
+        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
 end
 
 puts 'Event Manager Initialized!'
@@ -13,7 +37,13 @@ contents = CSV.open(
 )
 contents.each do |row|
     name = row[:first_name]
+
     zipcode = clean_zipcode(row[:zipcode])
 
-    puts "#{name} #{zipcode}"
+    legislators = legislators_by_zipcode(zipcode)
+
+    personal_letter = template_letter.gsub('FIRST_NAME', name)
+    personal_letter.gsub!('LEGISLATORS', legislators)
+    
+    puts personal_letter
 end
