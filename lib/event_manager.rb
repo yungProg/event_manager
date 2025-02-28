706ef5a5c451811be3f85619f60da501895b1cdc
yungProg <gboakye007@outlook.com>
2025-02-28 18:44:41 +0000

    nothing to commit

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
@@ -19,8 +20,13 @@ def clean_phone_number(phone_number)
     end
 end
 
-def most_active_date
-  
+def most_active_date(dates)
+    dates_str = dates.map do |date|
+        date = date.to_s
+        Date.parse(date).to_s
+    end
+    dates_str
+    #dates.group_by(&:itself).values.max_by(&:size).first
 end
 
 def legislators_by_zipcode(zipcode)
@@ -58,10 +64,12 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+reg_dates = []
 
 contents.each do |row|
     id = row[0]
     name = row[:first_name]
+    reg_dates.push(row[:regdate])
 
     zipcode = clean_zipcode(row[:zipcode])
 
@@ -73,3 +81,10 @@ contents.each do |row|
 
     save_thank_you_letter(id,form_letter)
 end
+
+# a = ['11/12/08 10:47', '11/12/08 13:23', '11/12/08 13:30']
+# # a.map do |bs|
+# #     p Date.parse(bs).to_s
+# # end
+
+p most_active_date(reg_dates)
